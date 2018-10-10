9503e5f896a65591e41f177f52f9c89bb39bb001
Dave Mateer <davemateer@gmail.com>
2018-10-10 10:59:59 +0100

    Add project files.

---
+++ Euler1Article/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Euler1Article
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
