8c409be35528adc38e335ff913b0459c95b7b5df
Dave Mateer <davemateer@gmail.com>
2019-02-14 15:25:31 +0000

    auto commit

---
+++ Example25Static/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Example25Static
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Example3HOF/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Example3HOF
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine($"3 : {IsPrime(3)}");
+            Console.WriteLine($"4 : {IsPrime(4)}");
+            Console.WriteLine($"5 : {IsPrime(5)}");
+        }
+
+        // Pure function - input (number) will always produce the same output (bool)
+        // as there is no shared state the function can be static
+        static bool IsPrime(int number)
+        {
+            for (long i = 2; i < number; i++)
+                if (number % i == 0)
+                    return false;
+            return true;
+        }
+    }
+}
